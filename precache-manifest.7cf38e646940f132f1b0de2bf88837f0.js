ee1382510ceec201968f7b84a54448fdfe00f479
Andreas Lee-Norman <andreas.lee-norman@knowit.se>
2020-09-30 14:47:49 +0200

    Updates

---
+++ precache-manifest.7cf38e646940f132f1b0de2bf88837f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49938ebdb37d2ac355ad626d8efe5449",
+    "url": "/flymenu-reactjs/index.html"
+  },
+  {
+    "revision": "6cfea17df6747d9b2765",
+    "url": "/flymenu-reactjs/static/js/2.19853a56.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/flymenu-reactjs/static/js/2.19853a56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1109fff764556162d71a",
+    "url": "/flymenu-reactjs/static/js/main.b8c1dae0.chunk.js"
+  },
+  {
+    "revision": "1cbfc0496d3ba7db4eee",
+    "url": "/flymenu-reactjs/static/js/runtime-main.11ef27bb.js"
+  },
+  {
+    "revision": "7d5e82ccff4af537509a29b300d830c0",
+    "url": "/flymenu-reactjs/static/media/arrow-right.7d5e82cc.svg"
+  },
+  {
+    "revision": "68cf3b20e3fff9b116e0e18cd780106e",
+    "url": "/flymenu-reactjs/static/media/menubars.68cf3b20.svg"
+  }
+]);
